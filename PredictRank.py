697ff3fdfa3c5de6834e5387288d9e075ddaa8aa
Jesse Min <jesikmin@stanford.edu>
2016-12-01 22:09:55 -0800

    add data pipelining and sample data for qucik test/learning

--- PredictRank.py
+++ PredictRank.py
@@ -1,23 +1,28 @@
 from rank_predictor import Predictor as Predictor
 from features import featureExtractor as FeatureExtractor
+from statsApi import APIRequester as APIRequester
 
 def main():
-    # 1) Get Data from LOL API 
+    # 1) Get Data from LOL API
     # TODO : import relevant modules and pull data as json format
+    # TODO : change sample data to the real data
+    sampleAPIRequester = APIRequester(playersFile="samplePlayerData")
+    sampleAPIRequester.writeToFile()
+    jsonData = sampleAPIRequester.readFromFile()
 
-    X = [] # list of dictionaries
-    Y = [] # list of values
-    #features = X[0].keys() # list of feature names in string. 
+    X = [] #list of dictionaries
+    Y = [] #list of values
+
+    for data in jsonData:
+        curRank = data.pop('curRank', None)
+        if not curRank: #edge case where the json data is malformed
+            pass
+        X.append(data)
+        Y.append(curRank)
+
     #TODO: Make sure X[0] has all attrs as keys. Else, add a manual list of all feature names
 
-    X = [
-    {u'totalPhysicalDamageDealt': 3488663, u'totalTurretsKilled': 52, u'totalSessionsPlayed': 74, u'totalAssists': 651, u'totalDamageDealt': 6379287, u'mostChampionKillsPerSession': 18, u'killingSpree': 159, u'totalPentaKills': 0, u'mostSpellsCast': 0, u'totalDoubleKills': 29, u'maxChampionsKilled': 18, u'totalQuadraKills': 1, u'totalDeathsPerSession': 367, u'totalSessionsWon': 39, u'totalGoldEarned': 773400, u'totalTripleKills': 3, u'totalNeutralMinionsKilled': 626, u'maxTimeSpentLiving': 1652, u'rankedPremadeGamesPlayed': 0, u'rankedSoloGamesPlayed': 0, u'maxLargestCriticalStrike': 1935, u'totalChampionKills': 321, u'maxNumDeaths': 12, u'totalMinionKills': 8349, u'totalMagicDamageDealt': 2661657, u'totalHeal': 335855, u'normalGamesPlayed': 0, u'maxLargestKillingSpree': 11, u'totalUnrealKills': 0, u'maxTimePlayed': 2795, u'totalDamageTaken': 1562841, u'botGamesPlayed': 0, u'totalSessionsLost': 35, u'totalFirstBlood': 0},
-{u'totalPhysicalDamageDealt': 25352467, u'totalTurretsKilled': 341, u'totalSessionsPlayed': 454, u'totalAssists': 4560, u'totalDamageDealt': 45458680, u'mostChampionKillsPerSession': 18, u'killingSpree': 1179, u'totalPentaKills': 0, u'mostSpellsCast': 0, u'totalDoubleKills': 165, u'maxChampionsKilled': 18, u'totalQuadraKills': 4, u'totalDeathsPerSession': 2161, u'totalSessionsWon': 239, u'totalGoldEarned': 5179999, u'totalTripleKills': 19, u'totalNeutralMinionsKilled': 16068, u'maxTimeSpentLiving': 2584, u'rankedPremadeGamesPlayed': 0, u'rankedSoloGamesPlayed': 0, u'maxLargestCriticalStrike': 1864, u'totalChampionKills': 2187, u'maxNumDeaths': 14, u'totalMinionKills': 33286, u'totalMagicDamageDealt': 16667479, u'totalHeal': 2975599, u'normalGamesPlayed': 0, u'maxLargestKillingSpree': 14, u'totalUnrealKills': 0, u'maxTimePlayed': 3761, u'totalDamageTaken': 11292612, u'botGamesPlayed': 0, u'totalSessionsLost': 215, u'totalFirstBlood': 0}
-    ]
-    Y = [13, 16]
     features = X[0].keys()
-
-
 
 
     # 2) Filter and add features
@@ -25,8 +30,6 @@ def main():
     f_extractor.feedData(X, Y)
 
     X, Y = f_extractor.separateXY(f_extractor.data)
-    print X
-    print Y
     predictor = Predictor(X, Y)
     predictor.setLearner('svm')
     predictor.learn()
---
+++ statsApi.py
@@ -0,0 +1,54 @@
+import requests
+import json
+import time
+
+
+API_KEY="f239b7ff-7cdb-464a-abf0-cfb2c1c7e287"
+SUMMONER_BY_NAME="https://na.api.pvp.net/api/lol/na/v1.4/summoner/by-name/"
+STATS="https://na.api.pvp.net/api/lol/na/v1.3/stats/by-summoner/"
+
+class APIRequester:
+    def __init__(self, playersFile=""):
+        self.key = API_KEY
+        self.players = []
+        self.playersFile = playersFile
+        self.jsonFileName = "lol-"+playersFile
+        self.outfile = open(self.jsonFileName, 'w')
+
+    def api_summonerIdByName(self, summonerName):
+        apiRequest=SUMMONER_BY_NAME+summonerName+"?api_key="+self.key
+        r = requests.get(apiRequest)
+        return str(r.json()[summonerName]["id"])
+
+    def api_stats(self, summonerId):
+        apiRequest=STATS+summonerId+"/ranked?season=SEASON2016&api_key="+self.key
+        r = requests.get(apiRequest)
+        for champion in r.json()["champions"]:
+            if champion["id"] == 0:
+                return champion["stats"]
+        return None
+
+    def writeToFile(self):
+        playersData = []
+        with open(self.playersFile, 'r') as pFile:
+            playersData = pFile.readlines()
+        for playerData in playersData:
+            player, prevRank, curRank = playerData.strip().split(" ")
+            print "=====Parsing summoner: {}, prevRank: {}, curRank: {}".format(player, prevRank, curRank)
+            stats = self.api_stats(self.api_summonerIdByName(player))
+            if not stats:
+                print "\t!!!API request for summoner: {} failed".format(player)
+                pass
+            stats["prevRank"] = int(prevRank)
+            stats["curRank"] = int(curRank)
+            self.players.append(stats)
+            time.sleep(1)
+        json.dump(self.players, self.outfile)
+        self.outfile.close()
+
+    #need to be run after the write to the file is complete or before its run
+    def readFromFile(self):
+        readJson = None
+        with open(self.jsonFileName, "r") as infile:
+            readJson = json.loads(infile.read())
+        return readJson
